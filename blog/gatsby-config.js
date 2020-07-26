d4bc0a9390fa4b9fe2d000da3253e20cd5e77536
sotaryoutarou <sotaryoutarou@gmail.com>
2020-07-26 22:17:45 +0900

    追加

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,15 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `ソタソタ草`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `ソタリョウタロウ`,
+      summary: ``,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    description: `ソタソタなるままに。いんたーねっとのお仕事してるエンジニアの雑多ブログ`,
+    siteUrl: `https://sotaryoutarou.github.io/`,
   },
   plugins: [
     {
@@ -60,7 +57,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
+        name: `ソタソタ草`,
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#ffffff`,

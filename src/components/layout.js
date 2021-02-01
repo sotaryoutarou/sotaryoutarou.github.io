f819adffc894993589eebc6c64cd5c9f629e21f8
sotaryoutarou <sotaryoutarou@gmail.com>
2021-02-02 00:40:52 +0900

    ページ上部の隙間を削除

--- src/components/layout.js
+++ src/components/layout.js
@@ -38,7 +38,7 @@ const Layout = ({ location, title, children }) => {
         marginLeft: `auto`,
         marginRight: `auto`,
         maxWidth: rhythm(24),
-        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        paddingBottom: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
       }}
     >
       <header>{header}</header>

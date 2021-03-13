60052790d4d24e2bd79802114441a5d8c54cfc54
sotaryoutarou <sotaryoutarou@gmail.com>
2021-03-13 23:58:17 +0900

    一旦コミット

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,107 +30,69 @@ const BlogIndex = ({ data, location }) => {
         <SEO title={siteTitle} />
 
         {posts.map(( nodes ) => {
-          const title_l = nodes[0].frontmatter.title || node[0].fields.slug;
-          const title_r = nodes[1].frontmatter.title || node[1].fields.slug;
+          const title_l = nodes[0].frontmatter.title || nodes[0].fields.slug;
+          const title_r = nodes[1].frontmatter.title || nodes[1].fields.slug;
 
-          return nodes.map(( post ) => {
-            return null;
+          return nodes.map(( post, index ) => {
+            let columnContainerStyle;
+            if (index == 0) {
+              columnContainerStyle = PostStyles.column__l__container
+              console.log("lllllllll");
+            } else {
+              console.log("rrrrrrrrrrr");
+              columnContainerStyle = PostStyles.column__r__container
+            }
+
+            return (
+              <div className={PostStyles.posts__container}>
+                <div className={columnContainerStyle}>
+                  <article key={post.fields.slug}>
+                    <div className="posts__image_container">
+                      <Link to={post.fields.slug}>
+                        <Image
+                          className="posts__image"
+                          fluid={post.frontmatter.hero.childImageSharp.fluid}
+                          imgStyle={{
+                            elevation:4,
+                            shadowOffset: { width: 5, height: 5 },
+                            shadowColor: "grey",
+                            shadowOpacity: 0.5,
+                            shadowRadius: 10,
+                          }}
+                        />
+                      </Link>
+                    </div>
+                    <div className={PostStyles.text__container}>
+                      <small className="posts__date">{post.frontmatter.date}</small>
+                      <header>
+                        <h3 className={PostStyles.title__content}>
+                          <Link
+                            className="posts__title__a"
+                            to={post.fields.slug}
+                          >
+                            {nodes[0].frontmatter.title}
+                          </Link>
+                        </h3>
+                      </header>
+                      <section>
+                        <p
+                          className="posts__desc"
+                          dangerouslySetInnerHTML={{
+                            __html: post.frontmatter.description || post.excerpt,
+                          }}
+                        />
+                        <div className={PostStyles.more__text__content}>
+                          <Link to={post.fields.slug}>
+                            続きを読む
+                          </Link>
+                        </div>
+                      </section>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            );
           })
-
-          return (
-            <div className={PostStyles.posts__container}>
-              <div className={PostStyles.column__l__container}>
-                <article key={node[0].fields.slug}>
-                  <div className="posts__image_container">
-                    <Link to={node[0].fields.slug}>
-                      <Image
-                        className="posts__image"
-                        fluid={node[0].frontmatter.hero.childImageSharp.fluid}
-                        imgStyle={{
-                          elevation:4,
-                          shadowOffset: { width: 5, height: 5 },
-                          shadowColor: "grey",
-                          shadowOpacity: 0.5,
-                          shadowRadius: 10,
-                        }}
-                      />
-                    </Link>
-                  </div>
-                  <div className={PostStyles.text__container}>
-                    <small className="posts__date">{node[0].frontmatter.date}</small>
-                    <header>
-                      <h3 className={PostStyles.title__content}>
-                        <Link
-                          className="posts__title__a"
-                          to={node[0].fields.slug}
-                        >
-                          {title_l}
-                        </Link>
-                      </h3>
-                    </header>
-                    <section>
-                      <p
-                        className="posts__desc"
-                        dangerouslySetInnerHTML={{
-                          __html: node[0].frontmatter.description || node[0].excerpt,
-                        }}
-                      />
-                      <div className={PostStyles.more__text__content}>
-                        <Link to={node[0].fields.slug}>
-                          続きを読む
-                        </Link>
-                      </div>
-                    </section>
-                  </div>
-                </article>
-              </div>
-              <div className={PostStyles.column__r__container}>
-                <article key={node[1].fields.slug}>
-                  <div className="posts__image_container">
-                    <Link to={node[1].fields.slug}>
-                      <Image
-                        className="posts__image"
-                        fluid={node[1].frontmatter.hero.childImageSharp.fluid}
-                        imgStyle={{
-                          elevation:4,
-                          shadowOffset: { width: 5, height: 5 },
-                          shadowColor: "grey",
-                          shadowOpacity: 0.5,
-                          shadowRadius: 10,
-                        }}
-                      />
-                    </Link>
-                  </div>
-                  <div className={PostStyles.text__container}>
-                    <small className="posts__date">{node[1].frontmatter.date}</small>
-                    <header>
-                      <h3 className={PostStyles.title__content}>
-                        <Link
-                          className="posts__title__a"
-                          to={node[1].fields.slug}
-                        >
-                          {title_r}
-                        </Link>
-                      </h3>
-                    </header>
-                    <section>
-                      <p
-                        className="posts__desc"
-                        dangerouslySetInnerHTML={{
-                          __html: node[1].frontmatter.description || node[1].excerpt,
-                        }}
-                      />
-                      <div className={PostStyles.more__text__content}>
-                        <Link  to={node[1].fields.slug}>
-                          続きを読む
-                        </Link>
-                      </div>
-                    </section>
-                  </div>
-                </article>
-              </div>
-            </div>
-          );
         })}
       </Layout>
     </div>
